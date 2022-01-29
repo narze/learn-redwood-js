1fa1ebd32b3de4c6a4436da19c4ff57e535cd91d
Manassarn Manoonchai <manassarn@gmail.com>
2022-01-29 20:06:30 +0700

    Add about & home page with blog layout

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -7,11 +7,16 @@
 // 'src/pages/HomePage/HomePage.js'         -> HomePage
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
 
-import { Router, Route } from '@redwoodjs/router'
+import { Router, Route, Set } from '@redwoodjs/router'
+import BlogLayout from 'src/layouts/BlogLayout/BlogLayout'
 
 const Routes = () => {
   return (
     <Router>
+      <Set wrap={BlogLayout}>
+        <Route path="/" page={HomePage} name="home" />
+        <Route path="/about" page={AboutPage} name="about" />
+      </Set>
       <Route notfound page={NotFoundPage} />
     </Router>
   )
